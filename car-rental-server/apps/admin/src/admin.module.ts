85a02297a1842de798e10c289a55f9645cb75cbc
vroom <1824970586@qq.com>
2022-11-16 11:56:17 +0800

    feat: 新增users接口

--- car-rental-server/apps/admin/src/admin.module.ts
+++ car-rental-server/apps/admin/src/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
