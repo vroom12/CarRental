64ad3bf914ae345cee5eb7006171d9237294e871
vroom <1824970586@qq.com>
2022-12-08 16:05:38 +0800

    重构

--- CarRental-UI/packages/stores/store.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const getDefaultStoreState = () => ({});
-
-export const useCoreStore = defineStore('core-data', {
-  state: () => getDefaultStoreState(),
-  getters: {},
-  actions: {},
-});
